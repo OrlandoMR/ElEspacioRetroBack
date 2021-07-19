b3d1a1a3e4c5570d8cc21a7596a60f2776a95038
Santiago Bonilla <ferneysantiago@gmail.com>
2021-07-19 11:04:27 -0500

    clase Categoria

---
+++ models/categoria.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const categoriaSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    }
+
+})
+
+module.exports = mongoose.model('categoria', categoriaSchema);
