9cc37eb96f75150aa8a53ce48c66001c2bcbff9c
OrlandoMR <orlando2170133@correo.uis.edu.co>
2021-07-19 10:35:56 -0500

    Prueba de error

--- models/comentario.model.js
+++ models/comentario.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const comentarioSchema = mongoose.Schema({
     DescripComent: {
         type: String,
-        required: true
+        required: false
     },
 
     Juego: {
